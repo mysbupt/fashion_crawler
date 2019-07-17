fdf5331c3e9006e8587cab4d255d3332c52cc0f0
mayunshan <mysbupt@gmail.com>
2019-07-17 13:24:37 +0800

    add the script to crawl user information

---
+++ crawl_user_info.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+import os
+import time
+import argparse
+import random
+import json
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+import urllib
+import requests
+
+import sys
+sys.path.insert(0, "./lib")
+from parse_userpage import parse_user_page
+
+import yaml
+
+
+def get_cmd():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--city", help="which city's users to crawl, city name")
+    parser.add_argument("-p", "--proxy", help="the socks5 proxy port")
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    paras = get_cmd()
+    proxy = paras.proxy
+    city = paras.city
+
+    city_stat = json.load(open("./city_stat.json"))
+    user_list = [] 
+    for user_name, post_cnt in sorted(city_stat[city]["user_stat"].items(), key=lambda i: i[1], reverse=True):
+        user_list.append(user_name)
+
+    res_file = "./data_userinfo/%s_json.txt" %(city)
+    finished_user = set()
+    if os.path.exists(res_file):
+        for line in open(res_file):
+            tmp_data = json.loads(line.strip())
+            finished_user.add(tmp_data["user_name"])
+    output = open(res_file, "a")
+
+    chrome_options = Options()
+    chrome_options.add_argument("--proxy-server=socks5://localhost" + ":" + proxy)
+    if proxy != "no":
+        browse_driver = webdriver.Chrome(chrome_options=chrome_options)
+    else:
+        browse_driver = webdriver.Chrome()
+    browse_driver.set_page_load_timeout(10)
+
+    cnt = len(finished_user)
+    for user_name in user_list:
+        if user_name in finished_user:
+            continue
+        cnt += 1
+        if cnt % 10 == 1:
+            print(cnt)
+
+        url = "https://www.instagram.com/" + user_name + "/"
+
+        browse_driver.get(url)
+        time.sleep(2)
+        try:
+            res = parse_user_page(browse_driver.page_source)
+        except:
+            print("bad url: %s, %s" %(city, url))
+            res = {}
+        res["user_name"] = user_name
+
+        output.write(json.dumps(res).encode("utf-8") + "\n")
+        time.sleep(random.randint(4, 6))
+
+
+if __name__ == '__main__':
+    main()
---
+++ lib/parse_userpage.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+import re
+import sys
+from bs4 import BeautifulSoup
+import argparse
+
+import datetime
+import requests
+import yaml
+
+
+def parse_num(ori_str):
+    num = None
+    if "," in ori_str:
+        ori_str = ori_str.replace(",", "")
+    if "K" in ori_str:
+        num = int(float(ori_str.split("K")[0]) * 1000)
+    if "M" in ori_str:
+        num = int(float(ori_str.split("M")[0]) * 1000000)
+    if num is None:
+        num = int(ori_str)
+
+    return num
+
+
+def parse_user_page(html_doc):
+    res = {}
+
+    soup = BeautifulSoup(html_doc, "lxml")
+
+    try:
+        desc = soup.find("meta", {"name": "description"})["content"]
+    except:
+        desc = "fail"
+
+    fans = -1
+    follows = -1
+    posts = -1
+    if desc != "fail":
+        x = desc.split(" - ")[0]
+        fans, follows, posts = x.split(u"、")
+        fans = fans.split(u"位粉丝")[0].strip()
+        fans = parse_num(fans)
+
+        follows = follows.split(u"已关注")[1].strip().split(u"人")[0].strip()
+        follows = parse_num(follows)
+
+        posts = posts.split(u"篇帖子")[0].strip()
+        posts = parse_num(posts)
+
+    res = {
+        "desc": desc,
+        "fans": fans,
+        "follows": follows,
+        "posts": posts
+    }
+
+    return res 
